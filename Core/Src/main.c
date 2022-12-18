c0944228f7f0d220d5135107d6859db8905b3cac
Jakub Dvořák <kubicekkuban@gmail.com>
2022-12-18 15:12:16 +0100

    init functions

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,87 +6,61 @@
 #define readBit(reg, bit) ((reg) & (1<<bit))
 
 
-/*
-int main(void)
-{
-
-  volatile i = 0;
-
-  setBit(RCC->AHB1ENR, 1);       // aktivace hodin brany GPIOA
-
-  setBit(GPIOB->MODER, 10);      // nastaveni pinu PA5 jako digital out
-  clrBit(GPIOB->MODER, 11);
-
-  clrBit(GPIOB->OTYPER, 5);    // nastaveni pinu PA5 jako dvoustavovy (push-pull)
-
-  setBit(GPIOB->OSPEEDR, 10);    // nastaveni rychlosti brany na medium speed
-  clrBit(GPIOB->OSPEEDR, 11);
-
-  clrBit(GPIOB->PUPDR, 10);   // vypnuti pull-up/pull-down
-  clrBit(GPIOB->PUPDR, 11);
-
-  setBit(GPIOB->ODR, 5);        // nastaveni logickych urovni pinu
-  clrBit(GPIOB->ODR, 5);
-
-  while (1){
-	  tglBit(GPIOB->ODR, 5);   // prepnuti stavu LED
-
-    for(i = 0; i < 500; i++);
-  }
-}
-
-
-
-*/
-
-
-int main(void){
-
+void pwmSetup(void){
 	setBit(RCC->APB1ENR, 0);        // Povoleni hodin casovace TIM2
 
-    TIM2->PSC = 15;                    // preddelicka 16 na 1 MHz
-    TIM2->ARR = 1000;                    // autoreload registr na 1000 Hz
-    TIM2->CCR2 = 250;
-    setBit(TIM2->EGR, 0);
+	TIM2->PSC = 15;                    // preddelicka 16 na 1 MHz
+	TIM2->ARR = 1000;                    // autoreload registr na 1000 Hz
+	TIM2->CCR2 = 250;
+	setBit(TIM2->EGR, 0);
+}
 
-
-
-    setBit(TIM2->CCMR1, 14);            // PWM1 mode pro CC2
-    setBit(TIM2->CCMR1, 13);            // PWM1 mode pro CC2
-    setBit(TIM2->CCER, 4);            // Povoleni vystupu CC2
+void pinSetup(void){
+	/*pwm pin PB3 setup*/
+	setBit(TIM2->CCMR1, 14);            // PWM1 mode pro CC2
+	setBit(TIM2->CCMR1, 13);            // PWM1 mode pro CC2
+	setBit(TIM2->CCER, 4);            // Povoleni vystupu CC2
 
 	setBit(RCC->AHB1ENR, 1);        // Povoleni hodin pro GPIOB
 
 
-    setBit(GPIOB->MODER, 7);        // Nastaveni AF pro PB3
-    clrBit(GPIOB->MODER, 6);        // Nastaveni AF pro PB3
-
-    //clrBit(GPIOB->MODER, 7);        // Nastaveni
-    //setBit(GPIOB->MODER, 6);        // Nastaveni
-
-    clrBit(GPIOB->PUPDR, 7);   // vypnuti pull-up/pull-down
-    clrBit(GPIOB->PUPDR, 6);
-
-    setBit(GPIOB->OSPEEDR, 6);    // nastaveni rychlosti brany na medium speed
-    clrBit(GPIOB->OSPEEDR, 7);
-
-    //clrBit(GPIOB->OTYPER, 3);
+	setBit(GPIOB->MODER, 7);        // Nastaveni AF pro PB3
+	clrBit(GPIOB->MODER, 6);        // Nastaveni AF pro PB3
 
 
-    setBit(GPIOB->AFR[0], 12);        // Nastaveni AF1 pro pin PB3
+	clrBit(GPIOB->PUPDR, 7);   // vypnuti pull-up/pull-down
+	clrBit(GPIOB->PUPDR, 6);
+
+	setBit(GPIOB->OSPEEDR, 6);    // nastaveni rychlosti brany na medium speed
+	clrBit(GPIOB->OSPEEDR, 7);
+
+	setBit(GPIOB->AFR[0], 12);        // Nastaveni AF1 pro pin PB3
+
+	/*Analog in */
+
+}
+
+
+
+int main(void){
+
+	pwmSetup();
+	pinSetup();
 
     clrBit(TIM2->SR, 0);             // Obnoveni status registru po prerueseni
     setBit(TIM2->CR1, 0);            // Povoleni casovace TIM2 CEN
-
-
     setBit(RCC->CR, 0);             // Nastaveni HSI jako zroj hodin (16MHz)
+
+
     int i = 0;
     while(1){
-
+    	/*
     	for(int k=0;k<10000;k++);
 		TIM2->CCR2 = i++;
 		if(i >1000)
-			i = 0;
+			i = 0;*/
+
+
     }
 
 }
